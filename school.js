23019f20c34b372864d99c085fd0b198bd18e7b1
Bibik Daniil <daniil.bibik@mail.ru>
2026-03-09 19:21:21 +0300

    Добавил файл а

--- school.js
+++ school.js
@@ -4,7 +4,7 @@ import Teacher from "./classTeacher.js"
 import Student from "./classStudent.js"
 
 const bookOne = new Book({
-    name: 'Жила была бабка 2',
+    name: 'Жила была бабка 3',
     author: 'Алекандр Бабкин', 
     language: 'Русский'
 })
