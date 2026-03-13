cc53ae4a985877362d8bb0a8d80659cfe262e837
Bibik Daniil <daniil.bibik@mail.ru>
2026-03-13 17:35:33 +0300

    Вернул название файлу variables.ts с уточнённым регистром

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import * as variables from "./variablesChanged.js"
+import * as variables from "./variables.js"
 
 variables.student1.books.add(variables.bookOne)
 variables.student1.books.add(variables.bookTwo)
